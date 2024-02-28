5ee9652af778c13981743e50025f1e49c79a0f5c
tomaz1502 <tomgm1502@gmail.com>
2024-02-28 20:43:02 +0100

    flag for choosing algorithm

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,18 @@
 open Basis.Util
 open Basis.Parser
 open Algs.Brute_force
-(* open Algs.Dpll *)
+open Algs.Dpll
 
 type mode = Parse | Solve
+type algo = Dpll | Brute_force
 
 let exec_mode = ref Solve
+let chosen_algo = ref Dpll
 let input_file = ref ""
 let output_file = ref ""
-let usage_msg = "tinysat --input=<path/to/input> [--output=<path/to/output>] [--only-parse]"
+let usage_msg = "tinysat --input=<path/to/input> [--output=<path/to/output>] [--only-parse] [--algo=<algo_name>]"
 
-let others = fun _ -> 
+let unexpected_cmd = fun _ -> 
   print_endline "Unexpected command line argument.";
   print_endline ("Usage: " ^ usage_msg);
   exit 1;;
@@ -18,12 +20,17 @@ let others = fun _ ->
 let speclist =
   [ ("--input", Arg.Set_string input_file, ": Path to input DIMACS file.")
   ; ("--output", Arg.Set_string output_file,
-     ": [Optional] Output file. If no file is specified, the output will be printed in stdout.")
+       ": [Optional] Output file. If no file is specified, the output will be printed in stdout.")
   ; ("--only-parse", Arg.Unit (fun _ -> exec_mode := Parse), ": Only parse input file and print the result.")
+  ; ("--algo", Arg.String (function
+                                         | "dpll"        -> chosen_algo := Dpll
+                                         | "brute_force" -> chosen_algo := Brute_force
+                                         | _             -> unexpected_cmd ()),
+       ": Set the solving algorithm. Available algorithms: `dpll`, `brute_force`. Default is `dpll`.")
   ]
 
 let () =
-  Arg.parse speclist others usage_msg;
+  Arg.parse speclist unexpected_cmd usage_msg;
   let input = read_file !input_file in
   let output_chan =
     match !output_file with
@@ -37,8 +44,12 @@ let () =
     | Parse ->
         Format.printf "Input:\n%a\n" print_dimacs dim;
     | Solve ->
-      brute_force dim |>
-      Format.printf "%a@\n" print_sat
+        let solution =
+          match !chosen_algo with
+          | Dpll -> dpll dim
+          | Brute_force -> brute_force dim
+        in
+        Format.printf "%a@\n" print_sat solution
     end
   | Error err ->
     Format.printf "[Parsing Error]: %s" err
