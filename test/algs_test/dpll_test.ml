76fd26965f4392a59288a32a762c7199f047b076
tomaz1502 <tomgm1502@gmail.com>
2024-03-24 16:32:07 +0100

    dpll tests

--- test/algs_test/dpll_test.ml
+++ test/algs_test/dpll_test.ml
@@ -8,7 +8,7 @@ let sat_test_from_instance_name i_name =
   let input = read_file ("../sat_cases/" ^ i_name) in
   match dimacs_from_string input with
     | Ok dimacs -> begin
-        match Brute_force.solve dimacs with
+        match Dpll.solve dimacs with
           | Ok assignment -> check_assignment dimacs assignment
           | Error _       -> false
     end
@@ -25,3 +25,9 @@ let%test "ex_01.dimacs should be SAT" =
 
 let%test "ex_02.dimacs should be SAT" =
   sat_test_from_instance_name "ex_02.dimacs"
+
+let%test "100vars_sat.dimacs should be SAT" =
+  sat_test_from_instance_name "100vars_sat.dimacs"
+
+let%test "uf20-01000.dimacs should be SAT" =
+  sat_test_from_instance_name "uf20-01000.dimacs"
