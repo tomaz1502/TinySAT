c1d4d415e5c2e106185df3022eed7d29658a3aea
tomaz1502 <tomgm1502@gmail.com>
2024-03-24 13:11:58 +0100

    more tests working for brute force

--- test/algs_test/brute_force_test.ml
+++ test/algs_test/brute_force_test.ml
@@ -4,13 +4,24 @@ open Lib.Util;;
 
 open Check_assignment;;
 
-let%test "sat1.dimacs should be SAT" =
-  let input = read_file "./data/sat1.dimacs" in
+let sat_test_from_instance_name i_name =
+  let input = read_file ("data/" ^ i_name) in
   match dimacs_from_string input with
     | Ok dimacs -> begin
         match solve dimacs with
           | Ok assignment -> check_assignment dimacs assignment
           | Error _       -> false
     end
-    | Error _err -> failwith "sorry"
+    | Error _ -> false
 
+let%test "sat1.dimacs should be SAT" =
+  sat_test_from_instance_name "sat1.dimacs"
+
+let%test "ex_00.dimacs should be SAT" =
+  sat_test_from_instance_name "ex_00.dimacs"
+
+let%test "ex_01.dimacs should be SAT" =
+  sat_test_from_instance_name "ex_01.dimacs"
+
+let%test "ex_02.dimacs should be SAT" =
+  sat_test_from_instance_name "ex_02.dimacs"
